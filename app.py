1f0a13d2ac6a7c2e2ce135fadfe25c499091bfe4
mike <mikehung0714@gmail.com>
2019-05-22 15:56:05 +0800

    我是佩佩豬機器人

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def callback():
 #接著透過LineBotApi物件中reply_message()方法，回傳相同的訊息內容
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text='我是佩佩豬機器人')
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
