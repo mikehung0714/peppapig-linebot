e2abdff81d562027e902cf8ee93e336eea362bff
mike <mikehung0714@gmail.com>
2019-06-12 16:19:41 +0800

    星座

--- function/astro.py
+++ function/astro.py
@@ -8,10 +8,15 @@ def star(n):
 
 def astroScore(select):
 	web = requests.get('https://m.click108.com.tw/astro/index.php?astroNum='+select)
+	web2 = requests.get('http://astro.click108.com.tw/unit002/2019/my2019.php?aid='+str(int(select)-1))
 	web.enconding = 'UTF-8'
+	web2.enconding = 'UTF-8'
 	global reply
 	reply = ''
 	soup = BeautifulSoup(web.text, "html.parser")
+	soup2 = BeautifulSoup(web2.text, "html.parser")
+	reply += '整體運勢 : ' + soup2.select('.score span')[0].text+'分\n'
+
 	reply += ('整體運')
 	star(int(soup.select('#astroDailyScore_all')[0]['style'][-6:-5:1]))
 	reply += (soup.select('#astroDailyData_all')[0].text)+'\n'
